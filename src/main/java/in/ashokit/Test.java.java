d55d40cdbcac380b5a386b448ef0dbfeff55bb05
saakib_minhaj <saakib148@gmail.com>
2024-11-27 11:52:14 +0530

    first commit for day

---
+++ src/main/java/in/ashokit/Test.java.java
@@ -0,0 +1,13 @@
+package in.ashokit;
+
+/**
+ * Hello world!
+ *
+ */
+public class Test 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
